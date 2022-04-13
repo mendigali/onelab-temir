07ddd665dd4bd8f51e09185dbf3555029198a4e3
mendigali <mendigalitemir13@gmail.com>
2022-04-13 15:43:36 +0600

    feat: counter function component

--- lecture1/src/App.tsx
+++ lecture1/src/App.tsx
@@ -2,21 +2,20 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [count, setCount] = useState(0);
 
   const increment = () => {
-    setCounter(count => count + 1);
+    setCount(count + 1);
   };
- 
+
   const decrement = () => {
-    if (counter <= 0) return setCounter(0);
-    setCounter(count => count - 1);
+    if (count > 0) setCount(count - 1);
   };
 
   return (
     <div className="App">
       <button onClick={increment}>Plus</button>
-      <p>{counter}</p>
+      <p>{count}</p>
       <button onClick={decrement}>Minus</button>
     </div>
   );
