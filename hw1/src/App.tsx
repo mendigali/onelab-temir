b391d2da6727d5f6d1204a6ba6576c4696731527
mendigali <mendigalitemir13@gmail.com>
2022-04-13 16:43:34 +0600

    feat: counter with input(function component)

--- hw1/src/App.tsx
+++ hw1/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, ChangeEvent } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setCount(Number(event.target.value));
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <label htmlFor="num">Number: </label>
+      <input type="number" name="num" id="num" onChange={inputHandler}/>
+      <br />
+      <button onClick={increment}>Plus</button>
+      <button onClick={decrement}>Minus</button>
     </div>
   );
 }
